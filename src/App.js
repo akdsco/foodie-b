2f6b5df78bde8c0cc0bc9cd0287cb1c4a169bdc4
akdsco <arkadiusz@ostrowski.co>
2019-07-09 07:17:33 +0100

    add google map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Map from "./Map";
+import './App.css';
 
 class App extends React.Component {
 
   render() {
     return (
         <div>
-          empty div
+          <Map />
+          {/*<Sidebar />*/}
         </div>
     );
   }
