1615ffd6dd4eb874fd8cb6c4af6671d62539bf16
Arkadiusz <arkadiusz@ostrowski.co>
2020-01-24 21:22:59 +0000

    clean up RestTitle component

--- src/components/RestTitle.js
+++ src/components/RestTitle.js
@@ -9,24 +9,31 @@ import {AvgRatingComponent} from "./RatingComponents";
 // Dependencies
 import {Button, Icon, Item } from "semantic-ui-react";
 
-const RestTitle = (props) => {
-  const photo = props.item.streetViewURL ? props.item.streetViewURL : (props.item.details.photoUrl ? props.item.details.photoUrl : foodPlate);
+export default function RestTitle(props) {
+  const {avgRating, active} = props;
+  const {restaurantName, streetViewURL, details, address} = props.item;
+
+  const photo = streetViewURL ? streetViewURL : (details.photoUrl ? details.photoUrl : foodPlate);
 
   return(
     <Item>
       <Item.Image src={photo} size='tiny' floated='left'/>
 
       <Item.Content>
-        <Item.Header as='a'>{props.item.restaurantName}</Item.Header>
+        <Item.Header as='a'>{restaurantName}</Item.Header>
         <Item.Meta>
-          <span className='restaurant'>{props.item.address}</span>
+          <span className='restaurant'>{address}</span>
         </Item.Meta>
         <Item.Extra>
-          <AvgRatingComponent avgRating={props.avgRating}/>
-          <Button className='more-info' size='mini' floated='right'>{props.active ? 'Less' : 'More Info'}<Icon name='dropdown'/></Button>
+          <AvgRatingComponent avgRating={avgRating}/>
+          <Button
+            className='more-info'
+            size='mini'
+            floated='right'
+          >
+            {active ? 'Less' : 'More Info'}<Icon name='dropdown'/>
+          </Button>
         </Item.Extra>
       </Item.Content>
     </Item>)
 };
-
-export default RestTitle
