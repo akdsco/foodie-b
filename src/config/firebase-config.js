2f5078685fa681dd0cb1dbf9b519fcf8ad8d7cbd
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-04 15:15:10 +0100

    add firebase-config.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBbjlslxy_rplj3giez7kEiDgF59WZWGg0",
+  authDomain: "akds-portfolio.firebaseapp.com",
+  databaseURL: "https://akds-portfolio.firebaseio.com",
+  projectId: "akds-portfolio",
+  storageBucket: "akds-portfolio.appspot.com",
+  messagingSenderId: "410903148637",
+  appId: "1:410903148637:web:accaa5fc2b46dfbdf4b5b4"
+};
