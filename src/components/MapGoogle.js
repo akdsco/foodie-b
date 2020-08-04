575822360a27802f0ab0c9fee30e14d00984d75f
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-04 13:24:25 +0100

    refactor props in MapGoogle.js

--- src/components/MapGoogle.js
+++ src/components/MapGoogle.js
@@ -1,39 +1,50 @@
-import runtimeEnv from "@mars/heroku-js-runtime-env";
-
-import React, {useState} from 'react';
+import React, { useState } from "react";
 // Dependencies
-import {GoogleMap, LoadScript, Marker, InfoWindow} from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 // Components
 import AddRest from "./AddRest";
 import RestMarker from "./RestMarker";
 // Icons
 import markerUserIcon from "../img/marker-user.png";
 import markerCenterIcon from "../img/marker-center.png";
-
+//Firebase
+const functions = require("firebase/functions");
 // Map Style
-const styles = require('../data/GoogleMapStyles.json');
+const styles = require("../data/GoogleMapStyles.json");
 
-// Production Environment
-const env = process.env.NODE_ENV === 'production' ? runtimeEnv() : process.env;
-const REACT_APP_G_API_KEY = env.REACT_APP_G_API_KEY;
-
-export default function MapGoogle(props) {
-  const {center, userMarker, openInfoWindow, closeInfoWindow, activeRest, handleActiveRest,
-         mapState, restaurants, handleNewData } = props;
-  const {lat, lng} = props.userLocation;
+export default function MapGoogle({
+  center,
+  userMarker,
+  openInfoWindow,
+  closeInfoWindow,
+  activeRest,
+  handleActiveRest,
+  handleCenterChange,
+  handleZoomChange,
+  mapState,
+  restaurants,
+  handleNewData,
+  userLocation,
+}) {
+  const { lat, lng } = userLocation;
   const [map, setMap] = useState();
 
   function onDragEnd() {
-    props.handleCenterChange( {
+    handleCenterChange({
       lat: map.getCenter().lat(),
-      lng: map.getCenter().lng()
+      lng: map.getCenter().lng(),
     });
   }
 
   function onZoomChanged() {
-    if(map){
+    if (map) {
       const zoom = map.getZoom();
-      props.handleZoomChange(zoom);
+      handleZoomChange(zoom);
     }
   }
 
@@ -41,18 +52,20 @@ export default function MapGoogle(props) {
     <div>
       <LoadScript
         id="script-loader"
-        googleMapsApiKey={REACT_APP_G_API_KEY}
+        googleMapsApiKey={functions.config().foodieb.mapskey}
       >
         <GoogleMap
-          onLoad={map => {setMap(map)}}
-          id='restaurants-map'
+          onLoad={(map) => {
+            setMap(map);
+          }}
+          id="restaurants-map"
           mapContainerStyle={{
             height: "100vh",
-            width: "100%"
+            width: "100%",
           }}
           zoom={15}
           center={center}
-          onRightClick={e => openInfoWindow(e)}
+          onRightClick={(e) => openInfoWindow(e)}
           onDragEnd={onDragEnd}
           onZoomChanged={onZoomChanged}
           options={{
@@ -61,60 +74,58 @@ export default function MapGoogle(props) {
             scaleControl: true,
             scrollWheel: true,
             draggable: true,
-            styles: styles
+            styles: styles,
           }}
         >
           {/* Load user Marker */}
-          {userMarker &&
-          <Marker
-            icon={{url: markerUserIcon}}
-            position={{lat, lng}}
-          />
-          }
+          {userMarker && (
+            <Marker icon={{ url: markerUserIcon }} position={{ lat, lng }} />
+          )}
 
           {/* Center Marker */}
-          {center && ((center.lat !== lat) && (center.lng !== lng)) &&
-          <Marker
-            defaultClickable={false}
-            icon={{url: markerCenterIcon}}
-            position={{lat: center.lat, lng: center.lng}}
-          />
-          }
+          {center && center.lat !== lat && center.lng !== lng && (
+            <Marker
+              defaultClickable={false}
+              icon={{ url: markerCenterIcon }}
+              position={{ lat: center.lat, lng: center.lng }}
+            />
+          )}
 
           {/* Load Restaurant Markers */}
-          {props.restaurants.map( (r, id) =>
+          {restaurants.map((r, id) => (
             <RestMarker
               key={id}
-              position={{lat: r.lat, lng: r.lng}}
+              position={{ lat: r.lat, lng: r.lng }}
               index={r.id}
               restaurant={r}
               activeRest={activeRest}
               handleActiveRest={handleActiveRest}
-            />)
-          }
+            />
+          ))}
 
           {/* Load Add Restaurant InfoWindow on right click */}
-          {mapState.isRestAddButtonDisplayed &&
-          <InfoWindow
-            position={{lat: mapState.newRestData.lat, lng: mapState.newRestData.lng}}
-            onCloseClick={closeInfoWindow}
-          >
-            <div>
-              <h4>Add restaurant</h4>
-              <p>{mapState.newRestData.address}</p>
-              <AddRest
-                restaurants={restaurants}
-                newRestData={mapState.newRestData}
-                closeInfoWindow={closeInfoWindow}
-
-                handleNewData={handleNewData}
-              />
-            </div>
-          </InfoWindow>
-          }
-
+          {mapState.isRestAddButtonDisplayed && (
+            <InfoWindow
+              position={{
+                lat: mapState.newRestData.lat,
+                lng: mapState.newRestData.lng,
+              }}
+              onCloseClick={closeInfoWindow}
+            >
+              <div>
+                <h4>Add restaurant</h4>
+                <p>{mapState.newRestData.address}</p>
+                <AddRest
+                  restaurants={restaurants}
+                  newRestData={mapState.newRestData}
+                  closeInfoWindow={closeInfoWindow}
+                  handleNewData={handleNewData}
+                />
+              </div>
+            </InfoWindow>
+          )}
         </GoogleMap>
       </LoadScript>
     </div>
-  )
-};
+  );
+}
