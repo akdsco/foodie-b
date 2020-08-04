bafdb41e787befc97cc04ed6d5ade45a9bd77c2d
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-04 15:19:19 +0100

    refactor AddRest.js

--- src/components/AddRest.js
+++ src/components/AddRest.js
@@ -1,90 +1,95 @@
-import runtimeEnv from "@mars/heroku-js-runtime-env";
 // Imports
-import React, {useState} from 'react';
+import React, { useState } from "react";
 // Images
-import logoImg from '../img/logo.png';
+import logoImg from "../img/logo.png";
 // Custom Hooks
 import useUpdate from "./hooks/useUpdate";
 // Dependencies
-import {Form, Checkbox, Button, Modal, Image, Header} from 'semantic-ui-react';
+import {
+  Form,
+  Checkbox,
+  Button,
+  Modal,
+  Image,
+  Header,
+} from "semantic-ui-react";
 
-// Production Environment
-const proxyPrefix = process.env.NODE_ENV === 'production' ? '/google-proxy' : '';
-const env = process.env.NODE_ENV === 'production' ? runtimeEnv() : process.env;
-const REACT_APP_G_API_KEY = env.REACT_APP_G_API_KEY;
+//Firebase
+const functions = require("firebase/functions");
 
 // TODO investigate why {} is supplied to isTermsChecked state instead of boolean
 
-export default function AddRest(props) {
+//TODO firebase cloud function to get data from GoogleMaps API -> Change in this file needed
+
+export default function AddRest({
+  closeInfoWindow,
+  newRestData,
+  restaurants,
+  handleNewData,
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isTermsChecked, setIsTermsChecked] = useUpdate(false);
+  const [isTermsChecked, setIsTermsChecked] = useUpdate({ value: false });
+  // Custom Hook
+  const [restName, setRestName] = useUpdate("");
+  const [phoneNumber, setPhoneNumber] = useUpdate("");
+  const [imageUrl, setImageUrl] = useUpdate("");
+  const [restUrl, setRestUrl] = useUpdate("");
 
-  const [restName, setRestName] = useUpdate('');
-  const [phoneNumber, setPhoneNumber] = useUpdate('');
-  const [imageUrl, setImageUrl] = useUpdate('');
-  const [restUrl, setRestUrl] = useUpdate('');
-
-  const {closeInfoWindow, newRestData, restaurants, handleNewData} = props;
-
-  function handleCancel () {
+  function handleCancel() {
     setIsModalOpen(false);
-    setIsTermsChecked({value: false});
-    props.closeInfoWindow();
+    setIsTermsChecked({ value: false });
+    closeInfoWindow();
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     const id = restaurants.length;
-    const {address, lat, lng} = newRestData;
+    const { address, lat, lng } = newRestData;
 
     const newRestaurant = {
-      "id": id,
-      "restaurantName": restName,
-      "address": address,
-      "lat": lat,
-      "lng": lng,
-      "streetViewURL": `${proxyPrefix}/maps/api/streetview?`+
-        `size=500x300`+
-        `&location=${lat},${lng}`+
-        `&heading=151.78`+
-        `&pitch=-0.76`+
-        `&key=${REACT_APP_G_API_KEY}`,
-      "isFromFile": true,
-      "place_id": "",
-      "avgRating": 0,
-      "numberOfReviews": 0,
-      "details": {
-        "reviews": [],
-        "photoUrl": imageUrl,
-        "phoneNumber": phoneNumber,
-        "link": restUrl,
-      }
+      id: id,
+      restaurantName: restName,
+      address: address,
+      lat: lat,
+      lng: lng,
+      streetViewURL:
+        `https://maps.googleapis.com/maps/api/streetview?` +
+        `size=500x300` +
+        `&location=${lat},${lng}` +
+        `&heading=151.78` +
+        `&pitch=-0.76` +
+        `&key=${functions.config().foodieb.mapskey}`,
+      isFromFile: true,
+      place_id: "",
+      avgRating: 0,
+      numberOfReviews: 0,
+      details: {
+        reviews: [],
+        photoUrl: imageUrl,
+        phoneNumber: phoneNumber,
+        link: restUrl,
+      },
     };
 
-    handleNewData(newRestaurant, 'restaurant');
+    handleNewData(newRestaurant, "restaurant");
     handleCancel();
   }
 
-  return(
+  return (
     <div>
-      {console.log('isTermsChecked: ' + isTermsChecked)}
+      {console.log("isTermsChecked: " + isTermsChecked)}
       <Button.Group>
-        <Button
-          onClick={setIsModalOpen}
-          positive
-          value={true}
-          name='modalOpen'
-        > Add </Button>
+        <Button onClick={setIsModalOpen} positive value={true} name="modalOpen">
+          {" "}
+          Add{" "}
+        </Button>
         <Button.Or />
         <Button onClick={closeInfoWindow}>Cancel</Button>
       </Button.Group>
 
-      <Modal
-        open={isModalOpen}
-        onClose={handleCancel}
-      >
+      <Modal open={isModalOpen} onClose={handleCancel}>
         <Modal.Content image>
-          <Image wrapped size='medium' src={logoImg} />
+          <Image wrapped size="medium" src={logoImg} />
 
           <Modal.Description>
             <Header>Add New Restaurant Details</Header>
@@ -93,7 +98,7 @@ export default function AddRest(props) {
                 <label>Restaurant Name</label>
                 <Form.Input
                   placeholder="e.g. Pizza Express (min. 4 characters)"
-                  name='restName'
+                  name="restName"
                   value={restName}
                   onChange={setRestName}
                 />
@@ -101,8 +106,8 @@ export default function AddRest(props) {
               <Form.Field>
                 <label>Image URL</label>
                 <Form.Input
-                  placeholder='e.g. http://mcdonalds.com/main-photo.jpg'
-                  name='imageUrl'
+                  placeholder="e.g. http://mcdonalds.com/main-photo.jpg"
+                  name="imageUrl"
                   value={imageUrl}
                   onChange={setImageUrl}
                 />
@@ -110,8 +115,8 @@ export default function AddRest(props) {
               <Form.Field>
                 <label>Phone Number</label>
                 <Form.Input
-                  placeholder='e.g. 07456066789'
-                  name='phoneNumber'
+                  placeholder="e.g. 07456066789"
+                  name="phoneNumber"
                   value={phoneNumber}
                   onChange={setPhoneNumber}
                 />
@@ -119,8 +124,8 @@ export default function AddRest(props) {
               <Form.Field>
                 <label>Restaurant Website</label>
                 <Form.Input
-                  placeholder='e.g. http://www.restaurant-name.com'
-                  name='restUrl'
+                  placeholder="e.g. http://www.restaurant-name.com"
+                  name="restUrl"
                   value={restUrl}
                   onChange={setRestUrl}
                 />
@@ -129,10 +134,12 @@ export default function AddRest(props) {
               <Form.Field required>
                 <Checkbox
                   checked={isTermsChecked}
-                  name='isTermsChecked'
+                  name="isTermsChecked"
                   onClick={setIsTermsChecked}
                   value={!isTermsChecked}
-                  label='I agree to the Terms and Conditions' required/>
+                  label="I agree to the Terms and Conditions"
+                  required
+                />
               </Form.Field>
               <Button.Group>
                 <Button
@@ -142,14 +149,12 @@ export default function AddRest(props) {
                   Submit
                 </Button>
                 <Button.Or />
-                <Button onClick={handleCancel}>
-                  Cancel
-                </Button>
+                <Button onClick={handleCancel}>Cancel</Button>
               </Button.Group>
             </Form>
           </Modal.Description>
         </Modal.Content>
       </Modal>
     </div>
-  )
+  );
 }
