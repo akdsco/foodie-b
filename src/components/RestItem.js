2f272c214b4b77e5456d417c5064b9ca4572a110
Arkadiusz <arkadiusz@ostrowski.co>
2020-08-04 13:35:16 +0100

    refactor RestItem.js

--- src/components/RestItem.js
+++ src/components/RestItem.js
@@ -1,67 +1,76 @@
 // Imports
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from "react";
 // Components
 import RestTitle from "./RestTitle";
-import RestItemCont from "./RestItemCont";
+import RestItemCont from "../containers/RestItemCont";
 // Dependencies
-import {Accordion} from "semantic-ui-react";
+import { Accordion } from "semantic-ui-react";
 
-export default function RestItem(props) {
-  const {activeRest, handleNewData, restaurant, windowWidth} = props;
+export default function RestItem({
+  activeRest,
+  handleNewData,
+  restaurant,
+  windowWidth,
+  handleActiveRest,
+}) {
   const [scrollFlag, setScrollFlag] = useState(true);
   const restItemRef = useRef(null);
 
   useEffect(() => {
-    if(activeRest === -1) {
+    if (activeRest === -1) {
       setScrollFlag(true);
     }
-    if(scrollFlag) {
-      if(activeRest === restaurant.id) {
+    if (scrollFlag) {
+      if (activeRest === restaurant.id) {
         scrollToItem();
         setScrollFlag(false);
       }
     }
-  },[activeRest, scrollFlag, restaurant.id]);
+  }, [activeRest, scrollFlag, restaurant.id]);
 
   function handleAccordionClick(e, titleProps) {
     scrollToItem();
-    const {index} = titleProps;
-    props.handleActiveRest(index);
+    const { index } = titleProps;
+    handleActiveRest(index);
   }
 
   function scrollToItem() {
-    setTimeout(() =>
-    restItemRef.current.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    }), 220);
+    setTimeout(
+      () =>
+        restItemRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        }),
+      220
+    );
   }
 
-  const {id, avgRating} = restaurant;
+  const { id, avgRating } = restaurant;
 
-  return(
+  return (
     <div ref={restItemRef}>
-      <Accordion className='mb-2' styled>
+      <Accordion className="mb-2" styled>
         <Accordion.Title
           active={activeRest === id}
           index={id}
-          onClick={handleAccordionClick}>
+          onClick={handleAccordionClick}
+        >
           <RestTitle
             active={activeRest === id}
             item={restaurant}
             avgRating={avgRating}
           />
         </Accordion.Title>
-        {activeRest === id &&
-        <Accordion.Content active={activeRest === id}>
-          <RestItemCont
-            restaurant={restaurant}
-            windowWidth={windowWidth}
-            handleNewData={handleNewData}
-          />
-        </Accordion.Content>
-        }
+        {activeRest === id && (
+          <Accordion.Content active={activeRest === id}>
+            <RestItemCont
+              restaurant={restaurant}
+              windowWidth={windowWidth}
+              handleNewData={handleNewData}
+            />
+          </Accordion.Content>
+        )}
       </Accordion>
     </div>
-  )
+  );
 }
